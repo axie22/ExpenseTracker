c7e43c3943e3dedfc9780c7e65a6d1afb577d5bf
axie22 <alexxie9667@gmail.com>
2024-09-17 15:28:42 -0400

    Model set up

---
+++ README.md
@@ -0,0 +1,15 @@
+# Expense Tracker | Backend
+
+# Table of Contents
+
+1. [Introduction](#introduction)
+2. [Setup](#setup)
+
+## Introduction <a name="intoduction"></a>
+
+## Setup <a name="setup"></a>
+
+Create a virtual environment
+
+Mac: `python3 -m venv venv` \
+Windows: `python -m venv venv`
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 from datetime import datetime
 
 client = MongoClient('mongodb://localhost:27017/')
@@ -6,6 +7,24 @@ db = client['expense_tracker']
 
 users = db['users']  
 expenses = db['expenses']
+
+def create_user(username, email, hash_password):
+    user = {
+        "username": username,
+        "email": email,
+        "password": hash_password,
+        "created_at": datetime.now()
+    }
+    return users.insert_one(user).inserted_id
+
+def delete_user(user_id):
+    # returns 1 if successful, 0 otherwise
+    return users.delete_one({"_id": ObjectId(user_id)})
+
+def find_username(email):
+    return users.find_one({"email": email})
+
+'''
 
 users.insert_one({
     "username": "testuser",
@@ -20,3 +39,5 @@ expenses.insert_one({
     "category": "Groceries",
     "date": datetime.now(),
 })
+
+'''
